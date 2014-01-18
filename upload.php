ef5f73e89cca4cdf8e2228e35dfd4064ae091652
Tasos <netplayer@gmx.com>
2014-01-18 02:43:58 +0200

    test commit

---
+++ upload.php
@@ -0,0 +1,23 @@
+<?php
+//error_reporting(E_ALL);
+
+if($_POST) {
+	
+    define('UPLOAD_DIR', 'upload/');
+    $img = $_POST['image'];
+       $img = str_replace('data:image/png;base64,', '', $img);
+   
+    $img = str_replace(' ', '+', $img);
+   
+    $dataimg = base64_decode($img);
+    
+    $nameimg= uniqid() ;
+
+    $fileimg = UPLOAD_DIR . $nameimg . '.png';
+    $successimg = file_put_contents($fileimg, $dataimg);
+
+     echo $nameimg;
+
+
+}
+?>
